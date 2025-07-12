a8f51bab84537bc1e0098a57925447cd4feb066b
Narek2008654 <122670122+Narek2008654@users.noreply.github.com>
2025-07-12 20:45:10 +0400

    simulator.py

--- simulator.py
+++ simulator.py
@@ -10,8 +10,9 @@ dt = 0
 center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 player1_pos = center.copy()
 player2_pos = center.copy()
-player1_pos.y += 50
-player2_pos.y -= 50
+d_f_c=(140**2-100)**(1/2)
+player1_pos.y += d_f_c-20
+player2_pos.y -= d_f_c
 
 while running:
     for event in pygame.event.get():
@@ -20,11 +21,11 @@ while running:
 
     screen.fill("white")
     pygame.draw.circle(screen, "yellow", center, 300)
-    pygame.draw.circle(screen, "black", center, 170)
-    pygame.draw.circle(screen, "red", center, 150)
+    pygame.draw.circle(screen, "white", center, 144)
+    pygame.draw.circle(screen, "black", center, 140)
     # Draw rectangles centered at player positions
-    pygame.draw.rect(screen, "blue", (player1_pos.x, player1_pos.y, 10, 10))
-    pygame.draw.rect(screen, "blue", (player2_pos.x, player2_pos.y, 10, 10))
+    pygame.draw.rect(screen, "red", (player1_pos.x-10, player1_pos.y, 20, 20))
+    pygame.draw.rect(screen, "red", (player2_pos.x-10, player2_pos.y, 20, 20))
     ex_player1_pos= player1_pos.copy()
     ex_player2_pos=player2_pos.copy()
     keys = pygame.key.get_pressed()
@@ -44,7 +45,7 @@ while running:
         player1_pos.x -= 100 * dt
     if keys[pygame.K_l]:
         player1_pos.x += 100 * dt
-    if player2_pos.x+8>=player1_pos.x>=player2_pos.x-8 and player2_pos.y+8>=player1_pos.y>=player2_pos.y-8:
+    if player2_pos.x+10>=player1_pos.x>=player2_pos.x-10 and player2_pos.y+10>=player1_pos.y>=player2_pos.y-10:
         player1_pos=ex_player1_pos.copy()
         player2_pos=ex_player2_pos.copy()
     pygame.display.flip()
